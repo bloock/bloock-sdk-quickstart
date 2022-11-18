161a56dbda6ffd627ce9754ae5c93434dcd61437
josepmdc <jdomingocatafal@gmail.com>
2022-11-18 12:21:09 +0100

    Add signature JS

---
+++ js/src/samples/signature/signature.ts
@@ -0,0 +1,36 @@
+import { Bloock, BloockClient, EcsdaSigner, RecordBuilder } from "@bloock/sdk";
+import { Config } from "../../utils/config"
+import { Logger } from "../../utils/logger";
+import { Sample } from "../../utils/sample"
+
+Sample.run("signature", async (config: Config) => {
+    Bloock.setApiKey(config.apiKey);
+    let client = new BloockClient();
+
+    let keys = await client.generateKeys();
+
+    let signedRecord = await RecordBuilder
+        .fromString("Hello world")
+        .withSigner(new EcsdaSigner(keys.privateKey))
+        .build();
+
+    Logger.success("Record was signed successfully");
+
+    keys = await client.generateKeys();
+
+    Logger.info("Adding another signature");
+    signedRecord = await RecordBuilder
+        .fromRecord(signedRecord)
+        .withSigner(new EcsdaSigner(keys.privateKey))
+        .build();
+
+    Logger.success("Record was signed successfully");
+
+    Logger.success(`Hash: ${await signedRecord.getHash()}`)
+
+    let signatures = await signedRecord.getSignatures();
+
+    signatures.forEach((signature, i) => {
+        Logger.success(`Signature ${i+1}: ${signature.signature}`);
+    })
+})
