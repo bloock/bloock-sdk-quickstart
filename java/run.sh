df9362532700b68d5b7738bbe0f95064cd38a3b7
josepmdc <jdomingocatafal@gmail.com>
2022-11-24 12:17:45 +0100

    Fix maven stack overflow on compile error

--- java/run.sh
+++ java/run.sh
@@ -1,2 +1,2 @@
 DIR=$2;
-mvn -q compile assembly:single -Dname=$3 -Dpackage=com.bloock.quickstart.samples.${DIR//\//.}.$3;
+MAVEN_OPTS=-Xss2M mvn -q compile assembly:single -Dname=$3 -Dpackage=com.bloock.quickstart.samples.${DIR//\//.}.$3;
